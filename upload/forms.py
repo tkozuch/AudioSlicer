70fbe4b5f74be463cafcdcfe662583552fbcaf1c
Your Name <me@example.com>
2018-10-19 14:49:23 +0200

    --allow-empty

--- upload/forms.py
+++ upload/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     sample_text = """1- How To Fly 0:00
-2- Rampampam 01:22
-3- Clouds & Cream 02:22
-4- Australia Street 03:17
-5- These Girls 04:00"""
+2- Rampampam 0:12
+3- Clouds & Cream 00:22
+4- Australia Street 00:32
+5- These Girls 00:42"""
     title = forms.CharField(widget=forms.Textarea, label='',
                             initial=sample_text)
     file = forms.FileField(label='')
