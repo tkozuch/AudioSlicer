64aa30721a596d457d543f4f0e3a64492b1d57fe
Your Name <me@example.com>
2018-10-19 20:04:34 +0200

    --allow-empty

--- file_upload/settings.py
+++ file_upload/settings.py
@@ -142,13 +142,13 @@ try:
 except KeyError:
 	CELERY_RESULT_BACKEND = 'amqp://admin:admin@localhost:5672/admin_host'
 	
-broker_pool_limit = 1 # Will decrease connection usage
-broker_heartbeat = None # We're using TCP keep-alive instead
-broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
-result_backend = None # AMQP is not recommended as result backend as it creates thousands of queues
-event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
-worker_prefetch_multiplier = 1 # Disable prefetching, it's causes problems and doesn't help performance
-worker_concurrency = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
+# broker_pool_limit = 1 # Will decrease connection usage
+# broker_heartbeat = None # We're using TCP keep-alive instead
+# broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
+# result_backend = None # AMQP is not recommended as result backend as it creates thousands of queues
+# event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
+# worker_prefetch_multiplier = 1 # Disable prefetching, it's causes problems and doesn't help performance
+# worker_concurrency = 50 # If you tasks are CPU bound, then limit to the number of cores, otherwise increase substainally
 
 	
 CELERY_ACCEPT_CONTENT = ['json']
