35eabcc7509d1d5a69e7293ce093a303e930bfd9
Your Name <me@example.com>
2018-10-19 12:14:27 +0200

    Adjust buildpacks on Heroku

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AudioSlicer',
+    version='1.0',
+    packages=['upload', 'upload.migrations', 'file_upload'],
+    url='',
+    license='',
+    author='Tomasz Kożuch',
+    author_email='kozuch.tomasz1@gmail.com',
+    description='Web application for slicing audio material.'
+)
