f8ec7b2a3032139843780cc213ffb43b6a4fefb6
Your Name <me@example.com>
2018-10-21 19:56:14 +0200

    REDIS_URL in celery conf.

--- file_upload/celery.py
+++ file_upload/celery.py
@@ -6,7 +6,7 @@ from . import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'file_upload.settings')
 
-app = Celery('file_upload')
+app = Celery('file_upload', broker_pool_limit=1)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
