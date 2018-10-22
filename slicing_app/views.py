3afaa37b6fe48c25df9a5e8a2ce47d74791aa104
Your Name <me@example.com>
2018-10-22 22:38:04 +0200

    Change boto3 client region to eu-central-1.

--- slicing_app/views.py
+++ slicing_app/views.py
@@ -61,7 +61,8 @@ def sign_s3(request):
     file_name = request.GET.get('file_name')
     file_type = request.GET.get('file_type')
     
-    s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
+    s3 = boto3.client('s3', region_name='eu-central-1', config=Config(
+        signature_version='s3v4'))
     
     presigned_post = s3.generate_presigned_post(
         Bucket = S3_BUCKET,
