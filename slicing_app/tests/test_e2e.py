9c48e0dd84eadb4e7c8bd0d1cbce3eb2d0119e18
tkozuch <kozuch.tomasz1@gmail.com>
2021-03-12 23:52:48 +0100

    tests(): Fix tests to use live_server_url

--- slicing_app/tests/test_e2e.py
+++ slicing_app/tests/test_e2e.py
@@ -1,26 +1,29 @@
 import os
 
-from django.contrib.staticfiles.testing import LiveServerTestCase
+from django.conf import settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
-try:
-    browser_driver = os.environ["BROWSER_DRIVER"]
-except KeyError:
-    raise ValueError(
-        "BROWSER_DRIVER env variable not specified. \nPlease specify path to your "
-        "browsers driver, more information: "
-        "https://selenium-python.readthedocs.io/installation.html#drivers"
-    )
 
-os.environ["PATH"] += browser_driver
-
-
-class TestMostCommonUserActions(LiveServerTestCase):
+class TestMostCommonUserActions(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
+        try:
+            browser_driver = os.environ["BROWSER_DRIVER"]
+        except KeyError:
+            raise ValueError(
+                "BROWSER_DRIVER env variable not specified. \nPlease specify path to your "
+                "browsers driver, more information: "
+                "https://selenium-python.readthedocs.io/installation.html#drivers"
+            )
+
+        os.environ["PATH"] += browser_driver
         super().setUpClass()
         cls.selenium = webdriver.Chrome()
         cls.selenium.implicitly_wait(10)
+
+        if settings.DEBUG is False:
+            settings.DEBUG = True
 
     @classmethod
     def tearDownClass(cls):
@@ -28,21 +31,20 @@ class TestMostCommonUserActions(LiveServerTestCase):
         super().tearDownClass()
 
     def test_page_rendered(self):
-        # Fixme: To be changed to LiveServerTestCase url.
-        self.selenium.get("http://127.0.0.1:8000/")
+        self.selenium.get(self.live_server_url)
         self.selenium.find_element_by_xpath('//a[contains(text(),"Audio Slicer")]')
         self.selenium.find_element_by_xpath('//button[contains(text(), "Slice")]')
 
     def test_user_is_able_to_substract_inputs(self):
-        self.selenium.get("http://127.0.0.1:8000/")
+        self.selenium.get(self.live_server_url)
         self._delete_all_possible_inputs()
 
     def test_user_is_able_to_add_inputs(self):
-        self.selenium.get("http://127.0.0.1:8000/")
+        self.selenium.get(self.live_server_url)
         self._add_n_inputs(2)
 
     def test_user_is_able_to_choose_file(self):
-        self.selenium.get("http://127.0.0.1:8000/")
+        self.selenium.get(self.live_server_url)
         file = os.path.join(
             os.getcwd(), "slicing_app", "tests", "test_album_shorter.mp3"
         )
